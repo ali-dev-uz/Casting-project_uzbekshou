745b87326dcd131328f06fb4f4c36ce7827b4e1c
Ali <sevararuzieva2023@gmail.com>
2023-10-31 12:49:11 +0500

    New project

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from utils.set_bot_commands import set_default_commands
 async def on_startup(dispatcher):
     schedule = AsyncIOScheduler()
     await db.create()
-    await db.drop_users()
+    # await db.drop_users()
     # await db.drop_static()
-    await db.drop_data()
+    # await db.drop_data()
     await db.create_table_users()
     await db.create_data_pay_message()
     await db.create_data_static()
